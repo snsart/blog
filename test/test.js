91635cc224e3632fdf92e8c65ff36d7f95288909
snsart <snsart@163.com>
2019-01-07 20:22:52 +0800

    add

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+/*test1*/
 var model="<a>{#demo#}</a><h>{#head#}<h><p>{#info#}<p>";
 var data={
 	demo:"this is a demo",
@@ -6,8 +7,19 @@ var data={
 }
 function formateString(model,data){
 	return model.replace(/\{#(\w+)#\}/g,function(match,key){
-		return typeof data[dd]===undefined?"":data[dd];
+		return typeof data[key]===undefined?"":data[key];
 	});
 }
 
 formateString(model,data);
+
+/*test2*/
+
+var a={
+	xx:"你好11",
+	callXX:function(){
+		console.log(this.xx);
+	}
+}
+a.callXX();
+var b=a.callXX;
