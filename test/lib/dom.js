bace401c80f993c1694d8b01248a1a82864af3b6
snsart <snsart@163.com>
2019-01-14 16:35:34 +0800

    add

---
+++ test/lib/dom.js
@@ -0,0 +1,8 @@
+F.module('lib/dom',function(){
+	return{
+		fn1:function(id){
+			return "test123456";
+		}
+	}
+});
+console.log("jiazai....");
--- test/test.js
+++ test/test.js
@@ -14,12 +14,90 @@ function formateString(model,data){
 formateString(model,data);
 
 /*test2*/
-
-var a={
-	xx:"你好11",
-	callXX:function(){
-		console.log(this.xx);
+/*异步模块模式*/
+(function(F){
+	var moduleCache={},
+		setModule=function(moduleName,params,callback){
+			var _module,fn;
+			if(moduleCache[moduleName]){
+				_module=moduleCache[moduleName];
+				_module.status='loaded';
+				_module.exports=callback?callback.apply(_module,params):null;
+				while(fn=_module.onload.shift()){
+					fn(_module.exports);
+				}
+			}else{
+				callback&&callback.apply(null,params);
+			}
+		},
+		getURL=function(moduleName){
+			return String(moduleName).replace(/\.js$/g,'')+'.js';
+		},
+		loadScript=function(src){
+			var _script=document.createElement('script');
+			_script.type='text/javascript';
+			_script.charset='UTF-8';
+			_script.async=true;
+			_script.src=src;
+			document.getElementsByTagName('head')[0].appendChild(_script);
+		},
+		loadModule=function(moduleName,callback){
+			var _module;
+			if(moduleCache[moduleName]){
+				_module=moduleCache[moduleName];
+				if(_module.status==='loaded'){
+					setTimeout(callback(_module.exports),0);
+				}else{
+					_module.onload.push(callback);
+				}
+			}else{
+				moduleCache[moduleName]={
+					moduleName:moduleName,
+					status:'loading',
+					exports:null,
+					onload:[callback]
+				};
+				loadScript(getURL(moduleName));
+			}
+		};
+		
+	F.module=function(url,modDeps,modCallback){
+		var args=[].slice.call(arguments),
+			callback=args.pop(),
+			deps=(args.length&&args[args.length-1] instanceof Array)?args.pop():[],
+			url=args.length?args.pop():null,
+			params=[],
+			depsCount=0,
+			i=0,
+			len;
+		if(len=deps.length){
+			while(i<len){
+				(function(i){
+					depsCount++;
+					console.log(depsCount);
+					loadModule(deps[i],function(mod){
+						params[i]=mod;
+						depsCount--;
+						console.log(depsCount);
+						if(depsCount===0){
+							console.log("完成");
+							setModule(url,params,callback);	
+						}
+					});
+				})(i);
+				i++;
+			}
+		}else{
+			setModule(url,[],callback);
+			console.log("555555");
+		}
 	}
-}
-a.callXX();
-var b=a.callXX;
+	
+	
+})(
+	window.F={}
+)
+
+F.module(['lib/dom','lib/test2'],function(){
+	
+})
