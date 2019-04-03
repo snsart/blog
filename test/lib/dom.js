bf1298252b200b20c7a0f23534f442d4b2914a09
jhlwork <903473112@qq.com>
2019-04-03 13:43:58 +0800

    mod

--- test/lib/dom.js
+++
@@ -1,8 +0,0 @@
-F.module('lib/dom',function(){
-	return {
-		fn1:function(id){
-			return "test123456";
-		}
-	}
-});
-console.log("jiazai....");
--- test/lib/test2.js
+++
@@ -1,7 +0,0 @@
-F.module('lib/test2',function(){
-	return {
-		fn1:function(id){
-			return "test123456";
-		}
-	}
-});
